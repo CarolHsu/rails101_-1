0765b47af432bcdac7323158bcdb9e60ab346807
Carol Hsu <sheseee@gmail.com>
2012-07-03 11:21:45 +0800

    To change controller to no-scaffold style

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,31 +1,15 @@
 class BoardsController < ApplicationController
-	
+
 	def index
 		@boards = Board.all
-
-		respond_to do |format|
-			format.html # index.html.erb
-			format.json {render json: @boards}			
-		end
 	end
 
 	def show
 		@board = Board.find(params[:id])
-		@post = @board.posts.all
-
-		respond_to do |format|
-			format.html # show.html.erb
-			format.json {render json: @board}
-		end
 	end
 
 	def new
 		@board = Board.new
-
-		respond_to do |format|
-			format.html # new.html.erb
-			format.json {render json: @board}
-		end
 	end
 
 	def edit
@@ -35,28 +19,20 @@ class BoardsController < ApplicationController
 	def create
 		@board = Board.new(params[:board])
 
-		respond_to do |format|
-			if @board.save
-				format.html {redirect_to @board, notice: 'Board was successfully created!'}
-				format.json {render json: @board, status: :created, location: @board}
-			else
-				format.html {render action: 'new'}
-				format.json {render json: @board.error, status: :unprocessable_entity}
-			end
+		if @board.save
+			redirect_to board_path(@board)
+		else
+			render action: 'new'
 		end
 	end
 
 	def update
 		@board = Board.find(params[:id])
 
-		respond_to do |format|
-			if @board.update_attributes(params[:board])
-				format.html {redirect_to @board, notice: 'Board was successfully updated!'}
-				format.json {render json: @board, status: :updated, location: @board}
-			else
-				format.html {render action: 'edit'}
-				format.json {render json: @board.error, status: :unprocessable_entity}
-			end
+		if @board.update_attributes(params[:board])
+			redirect_to board_path(@board)
+		else
+			render action: 'edit'
 		end
 	end
 
@@ -64,10 +40,6 @@ class BoardsController < ApplicationController
 		@board = Board.find(params[:id])
 		@board.destroy
 
-		respond_to do |format|
-			format.html {redirect_to boards_url}
-			format.json {render head :no_content}
-		end
+		redirect_to boards_url
 	end
-
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,73 +1,45 @@
 class PostsController < ApplicationController
 
 	def index
-		@board = Board.find(params[:board_id])
-		redirect_to board_path(@board)
+		@posts = Post.all
 	end
 
 	def show
-		@board = Board.find(params[:board_id])
-		@post = @board.posts.find(params[:id])
-
-		respond_to do |format|
-			format.html # show.html.erb
-			format.json {render json: @post}
-		end
+		@post = Post.find(params[:id])
 	end
 
 	def new
-		@board = Board.find(params[:board_id])
-		@post = @board.posts.build
-
-		respond_to do |format|
-			format.html # new.html.erb
-			format.json {render json: @post}
-		end
+		@post = Post.new
 	end
 
 	def edit
-		@board = Board.find(params[:board_id])
-		@post = @board.posts.find(params[:id])
+		@post = Post.find(params[:id])
 	end
 
 	def create
-		@board = Board.find(params[:board_id])
-		@post = @board.posts.build(params[:post])
+		@post = Post.new(params[:post])
 
-		respond_to do |format|
-			if @post.save
-				format.html {redirect_to board_post_path(@board, @post), notice: 'Post was successfully created!'}
-				format.json {render json: @post, status: :created, location: @post}
-			else
-				format.html {render action: 'new'}
-				format.json {render json: @post.error, status: :unprocessable_entity}
-			end
+		if @post.save
+			redirect_to board_path(@board)
+		else
+			render action: 'new'
 		end
 	end
 
 	def update
-		@board = Board.find(params[:board_id])
-		@post = @board.posts.find(params[:id])
+		@post = Post.find(params[:id])
 
-		respond_to do |format|
-			if @post.update_attributes(params[:post])
-				format.html {redirect_to board_post_path(@board, @post), notice: 'Post was successfully updated!'}
-				format.json {render @post, status: :updated, location: @post}
-			else
-				format.html {render action: 'edit'}
-				format.json {render json: @post.error, status: :unprocessable_entity}
-			end
+		if @post.update_attributes(params[:post])
+			redirect_to board_path(@board)
+		else
+			render action: 'edit'
 		end
 	end
 
 	def destroy
-		@board = Board.find(params[:board_id])
-		@post = @board.posts.find(params[:id])
+		@post = Post.find(params[:id])
 		@post.destroy
 
-		respond_to do |format|
-			format.html {redirect_to board_posts_path(@board, @post)}
-			format.json {render head :ok}
-		end
+		redirect_to posts_url
 	end
 end
